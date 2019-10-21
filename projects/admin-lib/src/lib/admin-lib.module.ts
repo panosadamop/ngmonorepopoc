e6db5d87227897ef61bc2ae56d2476b3ad3d3807
padamopoulos <info@dnacreative.gr>
2019-10-21 10:01:03 +0300

    first commit

---
+++ projects/admin-lib/src/lib/admin-lib.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AdminLibComponent } from './admin-lib.component';
+
+
+
+@NgModule({
+  declarations: [AdminLibComponent],
+  imports: [
+  ],
+  exports: [AdminLibComponent]
+})
+export class AdminLibModule { }
